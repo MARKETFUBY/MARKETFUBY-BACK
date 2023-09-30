f5cd2ff7448e1640f603ff837ac2cc3198cd3ff6
Soyeon-Cha <7103sy@naver.com>
2023-10-01 07:57:45 +0900

    Initial commit

---
+++ src/main/java/MARKETFUBY/MarketfubyApplication.java
@@ -0,0 +1,13 @@
+package MARKETFUBY;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketfubyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketfubyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/MARKETFUBY/MarketfubyApplicationTests.java
@@ -0,0 +1,13 @@
+package MARKETFUBY;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketfubyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
