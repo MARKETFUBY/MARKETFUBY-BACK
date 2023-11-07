2348a5f6af8bfb355a19750973c124eb3b2fe09a
Soyeon-Cha <7103sy@naver.com>
2023-11-07 13:53:09 +0900

    feat: 로그아웃 구현

--- src/main/java/MARKETFUBY/Member/controller/MemberController.java
+++ src/main/java/MARKETFUBY/Member/controller/MemberController.java
@@ -3,19 +3,19 @@ package MARKETFUBY.Member.controller;
 import MARKETFUBY.Member.dto.MemberJoinRequestDto;
 import MARKETFUBY.Member.dto.MemberLoginRequestDto;
 import MARKETFUBY.Member.dto.MemberLoginResponseDto;
+import MARKETFUBY.Member.dto.RefreshTokenRequestDto;
 import MARKETFUBY.Member.service.MemberService;
+import MARKETFUBY.Member.service.RefreshTokenService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/members")
 public class MemberController {
     private final MemberService memberService;
+    private final RefreshTokenService refreshTokenService;
 
     // 회원가입
     @PostMapping("/signup")
@@ -27,5 +27,12 @@ public class MemberController {
     @PostMapping("/login")
     public MemberLoginResponseDto login (@RequestBody MemberLoginRequestDto requestDto) {
         return memberService.login(requestDto.getFubyId(), requestDto.getPasswd());
+    }
+
+    // 로그아웃
+    @DeleteMapping("/logout")
+    public String logout(@RequestBody RefreshTokenRequestDto requestDto) {
+        refreshTokenService.deleteRefreshToken(requestDto.getRefreshToken());
+        return "성공적으로 로그아웃되었습니다!";
     }
 }
